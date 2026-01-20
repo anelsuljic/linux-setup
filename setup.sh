81fd23ea4ed6b08c8cf77637799f27997de76e58
Anel Ademovic <asuljic48@gmail.com>
2026-01-20 20:28:04 +0000

    Add setup script for simbolic links

--- setup.sh
+++ setup.sh
@@ -9,8 +9,11 @@ bash setups/setup-git/setup-git.sh
 echo "Installing graphics drivers ..."
 bash setups/setup-graphics/setup-graphics.sh
 
-echo "Reinstalling apps ..."
+echo "Installing user apps ..."
 bash setups/setup-apps/setup-apps.sh
 
 echo "Setting up programming languages ..."
 bash setups/setup-proglang/setup-proglang.sh
+
+echo "Creating simbolic links ..."
+bash setups/setup-simlinks/setup-simlinks.sh
---
+++ setups/setup-simlinks/setup-simlinks.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+echo "Creating simbolic link for gnome-text-editor named as gte ..."
+sudo ln -s /usr/bin/gnome-text-editor /usr/local/bin/gte
