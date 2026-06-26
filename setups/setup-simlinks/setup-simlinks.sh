e2f1dc3b70d1bfe0c9b93d8c792cc58075097d6b
Anel Ademovic <asuljic48@gmail.com>
2026-06-26 15:34:45 +0000

    Update setup-bashrc.sh and setup-simlinks.sh

--- setups/setup-simlinks/setup-simlinks.sh
+++ setups/setup-simlinks/setup-simlinks.sh
@@ -5,3 +5,6 @@ read -p "Do you want to set up symbolic links? [y/n]: " choice
 printf '%*s\n\n\n\n\n\n' 40 '' | tr ' ' '-'
 
 [[ "$choice" != "y" ]] && exit 0
+
+echo "Creating simbolic link for gnome-text-editor named as gte ..."
+sudo ln -s /usr/bin/gnome-text-editor /usr/local/bin/gte
