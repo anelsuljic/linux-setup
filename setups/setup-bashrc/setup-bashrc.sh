63ac2f6997220d4c47d7152e80568f7f28fce3d2
Anel Ademovic <asuljic48@gmail.com>
2026-09-22 21:11:16 +0200

    Update setup-bashrc.

--- setups/setup-bashrc/setup-bashrc.sh
+++ setups/setup-bashrc/setup-bashrc.sh
@@ -2,8 +2,9 @@
 
 
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
-SETUP_BASH="$SCRIPT_DIR/setup-bashrc.txt"
-BASHRC="$HOME/.bashrc_custom"
+SCRIPT_BASHRC="$SCRIPT_DIR/files/bashrc_custom"
+REAL_BASHRC="$HOME/.bashrc_custom"
+BASHRC="$HOME/.bashrc"
 
 printf '\n\n\n\n\n%*s\n' 40 '' | tr ' ' '-'
 read -p "Do you want to set up .bashrc? [y/n]: " choice
@@ -11,10 +12,7 @@ printf '%*s\n\n\n\n\n\n' 40 '' | tr ' ' '-'
 
 [[ "$choice" != "y" ]] && exit 0
 
-cat "$SETUP_BASH" > "$BASHRC"
-source "$BASHRC"
+[[ -f "$REAL_BASHRC" ]] && rm -rf "$REAL_BASHRC"
 
-echo "Note 1: remember to change the name of your ssh key on section"\
-     "\"# Autostart ssh\" from .bashrc_custom"
-     
-echo "Note 2: on section \"# Installation of packages\" from .bashrc_custom, remember to update <linux-setup-dir> with the actual directory where the repository of linux setup is locally stored"
+ln -s "$SCRIPT_BASHRC" "$REAL_BASHRC"
+source "$BASHRC"
