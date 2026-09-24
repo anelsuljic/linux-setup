f98820a760c139d79ee9613547e97ebd50b800de
Anel Ademovic <asuljic48@gmail.com>
2026-09-24 20:33:51 +0200

    Update setup-graphics

--- setups/setup-graphics/setup-graphics.sh
+++ setups/setup-graphics/setup-graphics.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
 
 printf '\n\n\n\n\n%*s\n' 40 '' | tr ' ' '-'
-read -p "Do you want to set up nvidia drivers? [y/n]: " choice
+read -p "Do you want to set up GPUs? [y/n]: " choice
 printf '%*s\n\n\n\n\n\n' 40 '' | tr ' ' '-'
 
 [[ "$choice" != "y" ]] && exit 0
