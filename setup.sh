bf8aa7ef42d9c56b31821f5725fc1b3e9ed3d5f0
Anel Ademovic <asuljic48@gmail.com>
2026-09-25 17:02:22 +0200

    Add base organisation for setup-hyprland.

--- setup.sh
+++ setup.sh
@@ -8,6 +8,7 @@ bash $SCRIPT_DIR/setups/setup-git/setup-git.sh
 bash $SCRIPT_DIR/setups/setup-apps/setup-apps.sh
 bash $SCRIPT_DIR/setups/setup-proglang/setup-proglang.sh
 bash $SCRIPT_DIR/setups/setup-graphics/setup-graphics.sh
+bash $SCRIPT_DIR/setups/setup-hyprland/setup-hyprland.sh
 # bash $SCRIPT_DIR/setups/setup-simlinks/setup-simlinks.sh
 # bash $SCRIPT_DIR/setups/setup-time/setup-time.sh
 # bash $SCRIPT_DIR/setups/setup-scripts/setup-scripts.sh
